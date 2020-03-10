98001bd882a56e614d9b63770b005b27c4843e61
Cooper Higgins <cooperhiggins@gmail.com>
2020-03-09 23:11:40 -0400

    added wow.min.js changed header font size

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,27 @@
 const text = document.querySelector('.text');
 const strText = text.textContent;
 const splitText = strText.split('');
-text.textContent = "";
+// text.textContent = "";
 for( let i=0; i < splitText.length; i++) {
     text.innerHTML += "<span>" + splitText[i] + "</span>";
 };
 
 let char = 0;
-let timer = setInterval(onTick, 50);
+let timer = setInterval(onTick, 150);
 
 function onTick() {
     const span = text.querySelectorAll('span')[char];
     span.classList.add('fade');
     char++
     if(char === splitText.length){
-        complete();
+        repeatEffect();
         return;
     }
+
+}
+
+function repeatEffect(){
+    onTick();
 }
 
 function complete() {
